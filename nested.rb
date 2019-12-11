333da5d1c0dfe618db85ccf83ebac486d7ca216b
Kailana Kahawaii <kailanakahawaii@gmail.com>
2019-12-11 02:51:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,12 +15,11 @@ def hopper
           :languages => ["C"]
         }
      }
-
+return programmer_hash[:grace_hopper]
 
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
 	programmer_hash = 
  		{
@@ -37,6 +36,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +55,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages] = "C"
+     return programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -79,8 +81,12 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+{:yukihiro_matsumoto => {
+   :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+  }
+}
+return programmer_hash[:yukihiro_matsumoto] = :known_for
 end
 
 def changing_alan
